9eaacda893ab0ebc4ee1ccdb24da69f484b81ffc
Ovidio Santos <olnsbr@gmail.com>
2024-01-20 02:21:28 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Se vc estiver lendo isso, de uma risadinha sem graça.</h1>
+    return <h1>Se vc estiver lendo isso, de uma risadinha sem graça. 😇 </h1>
 }
 
 export default Home;
