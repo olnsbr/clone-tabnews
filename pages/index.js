56fa8310640c018b1bc58986d14d04e2c534747d
Ovidio Santos <olnsbr@gmail.com>
2024-03-07 06:18:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Se vc estiver lendo isso, de uma risadinha sem graça. 😇 </h1>
+function Home() {
+  return <h1>Se vc estiver lendo isso, de uma risadinha sem graça. 😇 </h1>;
 }
 
 export default Home;
