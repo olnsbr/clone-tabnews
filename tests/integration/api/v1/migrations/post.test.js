7de96f0df72ec27bcb6fbf3f3c1c276caa418776
Ovidio Santos <olnsbr@gmail.com>
2025-02-16 00:09:01 +0000

    fix: fix lint errors

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -9,9 +9,12 @@ describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("for the first time", async () => {
-        const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response1.status).toBe(201);
 
         const responseBody = await response1.json();
@@ -20,9 +23,12 @@ describe("POST /api/v1/migrations", () => {
         expect(responseBody.length).toBeGreaterThan(0);
       });
       test("for the second time", async () => {
-        const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response2.status).toBe(200);
 
         const responseBody2 = await response2.json();
@@ -30,6 +36,6 @@ describe("POST /api/v1/migrations", () => {
         expect(Array.isArray(responseBody2)).toBe(true);
         expect(responseBody2.length).toBe(0);
       });
-    })
-  })
-})
+    });
+  });
+});
