224a96e5853dfff46936848c6f50489261e9197b
Ovidio Santos <olnsbr@gmail.com>
2025-02-24 02:12:34 +0000

    style: lint fix

--- pages/status/index.js
+++ pages/status/index.js
@@ -6,50 +6,49 @@ async function fetchAPI(key) {
   return body;
 }
 
-
 export default function StatusPage() {
-
-
   return (
     <>
       <h1>Status</h1>
       <UpdatedAt />
       <DatabaseStatus />
     </>
-  )
+  );
 }
 
 function UpdatedAt() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
-  })
+  });
 
-  let updatedAtText = "Carregando..."
+  let updatedAtText = "Carregando...";
 
   if (!isLoading && data) {
-    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR")
-
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
   }
 
-  return <div>Última atualização: {updatedAtText}</div>
-
+  return <div>Última atualização: {updatedAtText}</div>;
 }
 
 function DatabaseStatus() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
-    refreshInterval: 2000
-  })
+    refreshInterval: 2000,
+  });
 
-  let databaseStatusInfo = "Carregando..."
+  let databaseStatusInfo = "Carregando...";
 
   if (!isLoading && data) {
     databaseStatusInfo = (
       <>
         <div>Versão: {data.dependencies.database.version} </div>
-        <div>Conexões Abertas: {data.dependencies.database.opened_connections} </div>
-        <div>Conexões Abertas: {data.dependencies.database.max_connections} </div>
+        <div>
+          Conexões Abertas: {data.dependencies.database.opened_connections}{" "}
+        </div>
+        <div>
+          Conexões Abertas: {data.dependencies.database.max_connections}{" "}
+        </div>
       </>
-    )
+    );
   }
 
   return (
@@ -57,7 +56,5 @@ function DatabaseStatus() {
       <h2>Database</h2>
       <div>{databaseStatusInfo}</div>
     </>
-  )
-
-
+  );
 }
